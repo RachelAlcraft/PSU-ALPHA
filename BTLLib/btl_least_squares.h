12d3b7605ac72608849beaa268a5b8f8fbb18535
Rachel A <RachelAlcraft@gmail.com>
2020-02-22 14:04:19 +0000

    Fix1 BTL Building in VS 2019
    To get the file btl_least_squares.h to build I had to move the functions around so that the first called function is at the top.

--- BTLLib/btl_least_squares.h
+++ BTLLib/btl_least_squares.h
@@ -77,7 +77,84 @@ using std::numeric_limits;
 		       three dimensions (i.e. normal x,y,z coordinates).
 		       WARNING: if coordinates with varying dimensions are input
 		       then this may well not be detected."]*/
+			   //.............................................................................
 
+// Calculate the centroid of a set of x,y,z coordinates held in an n x 3 matrix.
+
+template <class ConstForwardIterator, class OutputIterator>
+OutputIterator centroid(ConstForwardIterator first, ConstForwardIterator last,
+	OutputIterator result)
+{
+	BTL_REAL reciprocal_n = 3.0 / (last - first);
+
+	for (; first != last; first += 3)
+	{
+		*result += *first;
+		*(result + 1) += *(first + 1);
+		*(result + 2) += *(first + 2);
+	}
+
+	*result *= reciprocal_n;
+	*(result + 1) *= reciprocal_n;
+	*(result + 2) *= reciprocal_n;
+
+	return (result + 3);
+}
+
+//.............................................................................
+template <class ForwardIterator, class OutputIterator>
+OutputIterator _kearsley_matrix(ForwardIterator begin1, ForwardIterator end1,
+	ForwardIterator begin2, ForwardIterator end2,
+	OutputIterator matfit)
+{
+	// Calculate centroids of two sets of coordinates
+	numeric_vector<> centroid1, centroid2;
+	centroid(begin1, end1, centroid1.begin());
+	centroid(begin2, end2, centroid2.begin());
+
+	// Set up matrix to be diagonalized
+
+	numeric_vector<> summ, diff;
+	BTL_REAL sumcentres1 = centroid1[0] + centroid2[0];
+	BTL_REAL sumcentres2 = centroid1[1] + centroid2[1];
+	BTL_REAL sumcentres3 = centroid1[2] + centroid2[2];
+	BTL_REAL diffcentres1 = centroid1[0] - centroid2[0];
+	BTL_REAL diffcentres2 = centroid1[1] - centroid2[1];
+	BTL_REAL diffcentres3 = centroid1[2] - centroid2[2];
+
+	for (; begin1 != end1; begin1 += 3, begin2 += 3)
+	{
+		summ[0] = *begin2 + *begin1 - sumcentres1;
+		summ[1] = *(begin2 + 1) + *(begin1 + 1) - sumcentres2;
+		summ[2] = *(begin2 + 2) + *(begin1 + 2) - sumcentres3;
+		diff[0] = *begin2 - *begin1 + diffcentres1;
+		diff[1] = *(begin2 + 1) - *(begin1 + 1) + diffcentres2;
+		diff[2] = *(begin2 + 2) - *(begin1 + 2) + diffcentres3;
+
+		*matfit += diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2];
+		*(matfit + 1) += summ[1] * diff[2] - diff[1] * summ[2];
+		*(matfit + 2) += diff[0] * summ[2] - summ[0] * diff[2];
+		*(matfit + 3) += summ[0] * diff[1] - diff[0] * summ[1];
+
+		*(matfit + 5) += summ[1] * summ[1] + summ[2] * summ[2] + diff[0] * diff[0];
+		*(matfit + 6) += diff[0] * diff[1] - summ[0] * summ[1];
+		*(matfit + 7) += diff[0] * diff[2] - summ[0] * summ[2];
+
+		*(matfit + 10) += summ[0] * summ[0] + summ[2] * summ[2] + diff[1] * diff[1];
+		*(matfit + 11) += diff[1] * diff[2] - summ[1] * summ[2];
+
+		*(matfit + 15) += summ[0] * summ[0] + summ[1] * summ[1] + diff[2] * diff[2];
+	}
+
+	*(matfit + 4) = *(matfit + 1);
+	*(matfit + 8) = *(matfit + 2);
+	*(matfit + 9) = *(matfit + 6);
+	*(matfit + 12) = *(matfit + 3);
+	*(matfit + 13) = *(matfit + 7);
+	*(matfit + 14) = *(matfit + 11);
+
+	return (matfit + 16);
+}
 template <class ConstForwardIterator1, class ConstForwardIterator2, class T>
 T lsqfit_rmsd(ConstForwardIterator1 begin1, ConstForwardIterator1 end1,
 	      ConstForwardIterator2 begin2, ConstForwardIterator2 end2,
@@ -102,6 +179,79 @@ T lsqfit_rmsd(ConstForwardIterator1 begin1, ConstForwardIterator1 end1,
 
     return init;
 }
+//.............................................................................
+// Create fitting rotation matrix from the eigen vector that corresponds to the
+// smallest eigen vector of Kearsley's matrix.
+
+template <class ForwardIterator, class OutputIterator>
+OutputIterator rotation_from_fit(ForwardIterator eigenmatrix, OutputIterator rot)
+{
+	BTL_REAL q1 = *(eigenmatrix + 3);
+	BTL_REAL q2 = *(eigenmatrix + 7);
+	BTL_REAL q3 = *(eigenmatrix + 11);
+	BTL_REAL q4 = *(eigenmatrix + 15);
+
+	// initialize rotation matrix
+	*rot = q1 * q1 + q2 * q2 - q3 * q3 - q4 * q4;
+	*(rot + 1) = 2.0 * (q2 * q3 + q1 * q4);
+	*(rot + 2) = 2.0 * (q2 * q4 - q1 * q3);
+
+	*(rot + 3) = 2.0 * (q2 * q3 - q1 * q4);
+	*(rot + 4) = q1 * q1 + q3 * q3 - q2 * q2 - q4 * q4;
+	*(rot + 5) = 2.0 * (q3 * q4 + q1 * q2);
+
+	*(rot + 6) = 2.0 * (q2 * q4 + q1 * q3);
+	*(rot + 7) = 2.0 * (q3 * q4 - q1 * q2);
+	*(rot + 8) = q1 * q1 + q4 * q4 - q2 * q2 - q3 * q3;
+
+	return (rot + 9);
+}
+
+//.............................................................................
+// Check that both input containers have the same number of elements and that
+// number is multiple of 3. Output the number of x,y,z positions
+
+template <class ForwardIterator1, class ForwardIterator2>
+BTL_INT check_3d_data(ForwardIterator1 begin1, ForwardIterator1 end1,
+	ForwardIterator2 begin2, ForwardIterator2 end2)
+{
+	BTL_INT ndata1 = 0;
+	while (begin1 != end1)
+	{
+		ndata1++;
+		begin1++;
+	}
+
+	if (ndata1 == 0)
+		FATAL_ERROR("There are no elements in 1st container");
+
+	if (ndata1 % 3 != 0)
+		FATAL_ERROR("The number of elements in the 1st container is not a "
+			"multiple of 3");
+
+	BTL_INT ndata2 = 0;
+	while (begin2 != end2)
+	{
+		ndata2++;
+		begin2++;
+	}
+
+	if (ndata1 == 0)
+		FATAL_ERROR("There are no elements in 2nd container");
+
+	if (ndata2 % 3 != 0)
+		FATAL_ERROR("The number of elements in the 2nd container is not a "
+			"multiple of 3");
+
+	if (ndata1 != ndata2)
+		FATAL_ERROR("There are different numbers of elements in the two input "
+			"containers");
+
+	return ndata1 / 3;
+}
+
+//.............................................................................
+
 
 //.............................................................................
 	    /**#: [Description="Least squares fit one set of coordinates to
@@ -156,115 +306,10 @@ T lsqfit(ForwardIterator begin1, ForwardIterator end1,
     return init;
 }  
 
-//.............................................................................
-// Calculate the centroid of a set of x,y,z coordinates held in an n x 3 matrix.
 
-template <class ConstForwardIterator, class OutputIterator>
-OutputIterator centroid(ConstForwardIterator first, ConstForwardIterator last,
-                        OutputIterator result)
-{
-    BTL_REAL reciprocal_n = 3.0 / (last - first);  
-
-    for(; first != last; first+=3)
-    {
-        *result += *first;
-        *(result+1) += *(first+1);
-        *(result+2) += *(first+2);
-    }
-
-    *result *= reciprocal_n;
-    *(result+1) *= reciprocal_n;
-    *(result+2) *= reciprocal_n;
-
-    return (result+3);
-}
 
 		
-//.............................................................................
-
-template <class ForwardIterator, class OutputIterator>
-OutputIterator _kearsley_matrix(ForwardIterator begin1, ForwardIterator end1,
-		                ForwardIterator begin2, ForwardIterator end2,
-			        OutputIterator matfit)
-{ 
-    // Calculate centroids of two sets of coordinates
-    numeric_vector<> centroid1,centroid2; 
-    centroid(begin1,end1,centroid1.begin());
-    centroid(begin2,end2,centroid2.begin());
-
-    // Set up matrix to be diagonalized
-
-    numeric_vector<> summ,diff;
-    BTL_REAL sumcentres1 = centroid1[0] + centroid2[0];
-    BTL_REAL sumcentres2 = centroid1[1] + centroid2[1];
-    BTL_REAL sumcentres3 = centroid1[2] + centroid2[2];
-    BTL_REAL diffcentres1 = centroid1[0] - centroid2[0];
-    BTL_REAL diffcentres2 = centroid1[1] - centroid2[1];
-    BTL_REAL diffcentres3 = centroid1[2] - centroid2[2];
-
-    for ( ; begin1 != end1; begin1+=3,begin2+=3)
-    {	
-    	summ[0] = *begin2     +  *begin1     - sumcentres1;
-    	summ[1] = *(begin2+1) +  *(begin1+1) - sumcentres2;
-    	summ[2] = *(begin2+2) +  *(begin1+2) - sumcentres3;
-    	diff[0] = *begin2     -  *begin1     + diffcentres1;
-    	diff[1] = *(begin2+1) -  *(begin1+1) + diffcentres2;
-    	diff[2] = *(begin2+2) -  *(begin1+2) + diffcentres3;
-	
-	*matfit      += diff[0]*diff[0] + diff[1]*diff[1] + diff[2]*diff[2];
-	*(matfit+1)  += summ[1]*diff[2] - diff[1]*summ[2];
-	*(matfit+2)  += diff[0]*summ[2] - summ[0]*diff[2];
-	*(matfit+3)  += summ[0]*diff[1] - diff[0]*summ[1];
-
-	*(matfit+5)  += summ[1]*summ[1] + summ[2]*summ[2] + diff[0]*diff[0];
-	*(matfit+6)  += diff[0]*diff[1] - summ[0]*summ[1];
-	*(matfit+7)  += diff[0]*diff[2] - summ[0]*summ[2];
-
-	*(matfit+10) += summ[0]*summ[0] + summ[2]*summ[2] + diff[1]*diff[1];
-	*(matfit+11) += diff[1]*diff[2] - summ[1]*summ[2];
-
-	*(matfit+15) += summ[0]*summ[0] + summ[1]*summ[1] + diff[2]*diff[2];
-    }
-
-	*(matfit+4) = *(matfit+1);
-	*(matfit+8) = *(matfit+2);
-	*(matfit+9) = *(matfit+6);
-	*(matfit+12) = *(matfit+3);
-	*(matfit+13) = *(matfit+7);
-	*(matfit+14) = *(matfit+11);
-	    
-    return (matfit+16);
-}
-
-//.............................................................................
-// Create fitting rotation matrix from the eigen vector that corresponds to the
-// smallest eigen vector of Kearsley's matrix.
-
-template <class ForwardIterator, class OutputIterator>
-OutputIterator rotation_from_fit(ForwardIterator eigenmatrix, OutputIterator rot)
-{
-    BTL_REAL q1=*(eigenmatrix+3);
-    BTL_REAL q2=*(eigenmatrix+7);
-    BTL_REAL q3=*(eigenmatrix+11);
-    BTL_REAL q4=*(eigenmatrix+15);
-
-    // initialize rotation matrix
-    *rot = q1*q1 + q2*q2 - q3*q3 - q4*q4;
-    *(rot+1) = 2.0 * (q2*q3 + q1*q4);
-    *(rot+2) = 2.0 * (q2*q4 - q1*q3);
-
-    *(rot+3) = 2.0 * (q2*q3 - q1*q4);
-    *(rot+4) = q1*q1 + q3*q3 - q2*q2 - q4*q4;
-    *(rot+5) = 2.0 * (q3*q4 + q1*q2);
-
-    *(rot+6) = 2.0 * (q2*q4 + q1*q3);
-    *(rot+7) = 2.0 * (q3*q4 - q1*q2);
-    *(rot+8) = q1*q1 + q4*q4 - q2*q2 - q3*q3;
-    
-    return (rot+9);
-}                                      
-
-    	     	
+                        	     	
 //.............................................................................
 	    	/**#: [Description="Calculate the root mean squared deviation
 		       of two sets of coordinates."] 
@@ -283,49 +328,6 @@ T rmsd(ConstForwardIterator begin1,ConstForwardIterator end1,
     return sqrt((3*init)/(end1-begin1));
 }
 
-
-//.............................................................................
-// Check that both input containers have the same number of elements and that
-// number is multiple of 3. Output the number of x,y,z positions
-
-template <class ForwardIterator1, class ForwardIterator2>
-BTL_INT check_3d_data(ForwardIterator1 begin1, ForwardIterator1 end1,
- 			    ForwardIterator2 begin2, ForwardIterator2 end2)
-{
-    BTL_INT ndata1=0;
-    while (begin1 != end1)
-    {
-    	ndata1++;
-    	begin1++;
-    }
-
-    if (ndata1 == 0)
-	FATAL_ERROR("There are no elements in 1st container");
-
-    if (ndata1%3 != 0)
-    	FATAL_ERROR("The number of elements in the 1st container is not a "   
-    	    	    "multiple of 3");
-    	    	    
-    BTL_INT ndata2=0;
-    while (begin2 != end2)
-    {
-    	ndata2++;
-    	begin2++;
-    } 
-
-    if (ndata1 == 0)
-	FATAL_ERROR("There are no elements in 2nd container");
-
-    if (ndata2%3 != 0)
-    	FATAL_ERROR("The number of elements in the 2nd container is not a "   
-    	    	    "multiple of 3");
-
-    if (ndata1 != ndata2)
-    	FATAL_ERROR("There are different numbers of elements in the two input "
-    	            "containers");
-    
-    return ndata1/3;
-}
 
  _BTL_END_NAMESPACE
 
